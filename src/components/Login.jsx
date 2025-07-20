47a3521ee0c150a34e601b5e4c25c6d7604f0460
Jeff97ares <92552303+Jeff97ares@users.noreply.github.com>
2025-07-20 12:09:47 -0500

    fix: update login endpoint to use API Gateway IP

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
         setSuccess('');
 
         try {
-            const response = await fetch('http://localhost:8080/auth/login', {
+            const response = await fetch('http://100.25.195.141/auth/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, password })
